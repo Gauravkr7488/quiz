2bc61a60448d6e4a9ed0a5df8ac47b106538efb3
Gaurav Kumar <gauravkr748800@gmail.com>
2024-11-16 04:38:01 +0530

    html and css done

---
+++ script.js
@@ -0,0 +1,16 @@
+const o1 = document.getElementById("option1");
+const o2 = document.getElementById("option2");
+const o3 = document.getElementById("option3");
+const o4 = document.getElementById("option4");
+const b = document.getElementById("next");
+const p = document.getElementById("playground");
+
+let a = 0;
+function next() {
+    a++;
+    if(a == 1){
+        p.document.innerhtml
+    }
+    
+}
+b.addEventListener("click", next, false);
